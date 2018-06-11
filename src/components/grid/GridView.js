b21cb1e4c371fe00c4403dbb297ad30b83d76ec1
bernard <bernardkllogjri@gmail.com>
2018-06-11 22:49:18 +0200

    animated grid

--- src/components/grid/GridView.js
+++ src/components/grid/GridView.js
@@ -1,42 +1,62 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import GridItem from './GridItem';
+import ScrollReveal from 'scrollreveal/dist/scrollreveal.min.js';
 
-export default class GridView extends Component{
 
-    constructor(props){
+export default class GridView extends Component {
+
+    constructor(props) {
         super(props);
-        this.state={
-            items:[]
+        this.state = {
+            items: [],
         }
+        window.sr =ScrollReveal({ reset: true });
     }
 
-    componentWillMount(){
+    componentDidMount() {
+        window.sr.reveal('.card',{distance:'70px',duration:600,origin:'bottom',scale:0});
+    }
+
+    componentWillMount() {
         let items = [
-            {id:1,title:"Nice",desc:"Very Nice",image:"/images/image.png"},
-            {id:2,title:"Nice",desc:"Very Nice",image:"/images/image.png"},
-            {id:3,title:"Nice",desc:"Very Nice",image:"/images/image.png"},
-            {id:4,title:"Nice",desc:"Very Nice",image:"/images/image.png"},
+            { id: 1, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 2, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 3, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 5, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 6, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 7, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 8, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 9, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 10, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 11, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 12, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 13, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 14, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 15, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 16, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 17, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 18, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 19, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+            { id: 20, title: "Nice", desc: "Very Nice", image: "/images/image.png" },
+
         ];
         this.setState({
             items
         });
     }
 
-    render(){
+    render() {
 
         return (
             <div className="row container">
-                {
-                    this.state.items.map(i => {
-                        return <GridItem
-                            key={i.id}                                     
-                            desc={i.desc}
-                            image={i.image}
-                        />
-                    })                    
-                }
+                {this.state.items.map(i => {
+                    return <GridItem
+                        key={i.id}
+                        desc={i.desc}
+                        image={i.image}
+                    />
+                })}
             </div>
         )
     }
-
 }
